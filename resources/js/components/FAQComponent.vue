5f96e17a51897cfc3879685b1532480ea17fbcf9
Kuldyaev Viacheslav <slavalion1@gmail.com>
2023-01-31 02:32:26 +0300

    faq page was added

---
+++ resources/js/components/FAQComponent.vue
@@ -0,0 +1,29 @@
+<template >
+    <div class="container">
+        <h1 class="text-center font-weight-bold">Frequently Asked Questions</h1>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "FAQComponent",
+    mounted() {
+        console.log(JSON.parse(this.dataResponse))
+    },
+    props: [ ],
+}
+</script>
+
+<style scoped>
+    h1{
+        width: 100%;
+        font-size: 32px;
+        line-height: 40px;
+        margin-top: 20px;
+        margin-bottom: 32px;
+        text-align: center;
+        font-weight: 600;
+    }
+</style>
