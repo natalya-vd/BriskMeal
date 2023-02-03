78d60d694bef7efbc34968cbfff5f15878e6cc16
Kuldyaev Viacheslav <slavalion1@gmail.com>
2023-02-03 03:46:44 +0300

    FAQ Page

--- resources/js/components/QuestionsBlock.vue
+++ resources/js/components/QuestionsBlock.vue
@@ -1,9 +1,19 @@
 <template>
-    <div  v-if="currBlock===item.id" class="questionsBlock">
+    <div v-if="currBlock===item.id" class="questionsBlock">
         <h3>{{ item.title }}</h3>
-        <div v-for="faq in faqs"  :key="faq.id">
-            <h5>Qestion:{{ faq.question }}</h5>
-            <h5>Answer:{{ faq.answer }}</h5>
+        <div v-for="faq in faqs"  :key="faq.id" class="accordion accordion-flush" >
+            <div class="accordion-item">
+                <h2 class="accordion-header" :id="'heading'+faq.id">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapse'+faq.id" aria-expanded="true" aria-controls="collapseOne">
+                    {{ faq.question }}
+                </button>
+                </h2>
+                <div :id="'collapse'+faq.id" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    {{ faq.answer }}
+                </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -27,5 +37,36 @@ export default {
 </script>
 
 <style scoped>
-   
+    h3{
+        text-align: left;
+        padding-left: 40px;
+        font-size: 20px;
+        line-height: 30px;
+        margin: 0px 0px 16px;
+        font-weight: 700;
+    }
+   .accordion-button{
+        color: rgb(51, 51, 51);
+        text-transform: none;
+        font-size: 16px;
+        line-height: 22px;
+        cursor: pointer;
+        font-weight: bold;
+   }
+   .accordion-button:not(.collapsed) {
+        background-color: white;
+        box-shadow: none;
+    }
+    .accordion-button::after {
+        background-image: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23fff'><path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/></svg>");
+        background-color: #42693d;
+        background-size: 80% 80%;
+        background-position-x: center;
+        background-position-y: center;
+        border-radius: 50%;
+    }
+   .accordion-body{
+        text-align: left;
+   }
 </style>
+
